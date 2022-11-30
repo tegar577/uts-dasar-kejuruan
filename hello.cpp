d9563ee252db9bdb197bb5473ba25a4c8c300622
tegar577 <114580577+tegar577@users.noreply.github.com>
2022-11-30 14:13:17 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    cout << "Nama:TegarZulian"<< endl;
+    cout <<"Kelas:xPPLG1"<< endl;
+    cout <<"============"<< endl;
+    cout <<"angka1=10"<< endl;
+    cout <<"angka2=5"<< endl;
+    cout <<"Hasil:"<<10+5<< endl;
+    cout <<"Hasil:"<<5000*6<< endl;
+    cout <<"Hasil:"<<20005-7839<< endl;
+    return 0;
+}
